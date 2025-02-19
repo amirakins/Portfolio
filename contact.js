850d9d991e113e3f97962b96d817410f71bd7a47
amirakins <70003484+amirakins@users.noreply.github.com>
2025-02-19 18:35:37 -0500

    Add files via upload

--- contact.js
+++ contact.js
@@ -1,23 +1,38 @@
-function sendMail(){
-    var params = {
-        name: document.getElementById("name").value,
-        email: document.getElementById("email").value,
-        message: document.getElementById("message").value,
-      };
-    
-      //const serviceID = "service_het46lc";
-      const serviceID = "service_agw2gng";
-      const templateID = "template_ihh63ke";
-    
-        emailjs.send(serviceID, templateID, params)
-        .then(res=>{
-            document.getElementById("name").value = "";
-            document.getElementById("email").value = "";
-            document.getElementById("message").value = "";
-            console.log(res);
-            alert("Your message sent successfully!!")
-    
-        })
-        .catch(err=>console.log(err));
-}
+const form = document.getElementById('form');
+const result = document.getElementById('result');
 
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+  const formData = new FormData(form);
+  const object = Object.fromEntries(formData);
+  const json = JSON.stringify(object);
+  result.innerHTML = "Please wait...";
+
+  fetch('https://api.web3forms.com/submit', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body: json
+  })
+  .then(async (response) => {
+    let json = await response.json();
+    if (response.status === 200) {
+      result.innerHTML = "Form submitted successfully";
+    } else {
+      console.log(response);
+      result.innerHTML = json.message;
+    }
+  })
+  .catch(error => {
+    console.log(error);
+    result.innerHTML = "Something went wrong!";
+  })
+  .then(function() {
+    form.reset();
+    setTimeout(() => {
+      result.style.display = "none";
+    }, 3000);
+  });
+});
